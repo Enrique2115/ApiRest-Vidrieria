ac34843e0e48f8797cfb026cd7262e352274ff9b
Luis2115 <lmorochofebres@gmail.com>
2021-05-06 18:34:47 -0500

    Modificando cadena de conexion para heroku

--- src/settings/keys.js
+++ src/settings/keys.js
@@ -1,4 +1,17 @@
 module.exports = {
+
+    //USUARIO:PASSWORD@HOST/BASEDATOS
+    //:fe5d6353@us-cdbr-east-03.cleardb.com/heroku_8f588349368bb77
+
+    database: {
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'b6dabfd433d2b9',
+        password: 'fe5d6353',
+        database: 'heroku_8f588349368bb77',
+        multipleStatements: true //permite enviar multiples sentencias SQL
+    }
+
+    /*
     database: {
         host: 'localhost',
         user: 'root',
@@ -6,4 +19,5 @@ module.exports = {
         database: 'sistema',
         multipleStatements: true //permite enviar multiples sentencias SQL
     }
+    */
 }
