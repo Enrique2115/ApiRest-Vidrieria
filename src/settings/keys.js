7eaf5aef43e91c6dd2cb68e31f16a904a689aaa8
Luis2115 <lmorochofebres@gmail.com>
2021-05-06 18:58:53 -0500

    Volviendo la conexion al Local para pruebas

--- src/settings/keys.js
+++ src/settings/keys.js
@@ -1,8 +1,16 @@
 module.exports = {
 
     //USUARIO:PASSWORD@HOST/BASEDATOS
-    //:fe5d6353@us-cdbr-east-03.cleardb.com/heroku_8f588349368bb77
 
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'sistema',
+        multipleStatements: true //permite enviar multiples sentencias SQL
+    }
+
+    /*
     database: {
         host: 'us-cdbr-east-03.cleardb.com',
         user: 'b6dabfd433d2b9',
@@ -11,13 +19,6 @@ module.exports = {
         multipleStatements: true //permite enviar multiples sentencias SQL
     }
 
-    /*
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'sistema',
-        multipleStatements: true //permite enviar multiples sentencias SQL
-    }
+    
     */
 }
